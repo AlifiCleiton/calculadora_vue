6a00a7389a02715eb276d86a3297f0ec6fffd75c
Alifi Cleiton <alificleiton123@gmail.com>
2021-06-08 13:31:52 -0300

    adiconando arquivos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Calculadora</h1>
+    <Calculator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Calculator from "./main/Calculator"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Calculator }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style >
+@font-face{
+  font-family:"RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
 }
+
+*{
+  font-family: "RobotoMono", monospace;
+}
+
+body{
+  margin:0;
+}
+
+#app{
+  display:flex;
+  flex-direction: column;
+  height:100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color:#fff;
+  background:linear-gradient(183.5deg, rgb(244, 173, 6) 18.6%, rgb(229, 251, 31) 119.9%);;
+}
+
 </style>
